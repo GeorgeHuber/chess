ac2f497cf2a6b8fff29c1c4173236c72083c0cfb
GeorgeHuber <36140322+GeorgeHuber@users.noreply.github.com>
2021-03-03 21:57:56 -0600

    added pieces

---
+++ src/components/board.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export class Board extends React.Component{
+    render(){
+        return(
+            <div>
+            </div>
+        )
+    }
+} 
+
+
---
+++ src/screens/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import {Board} from "../components/board"
+class Home extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>Hello this is my home page</h1>
+                <Board/>
+            </div>
+        )
+    }
+
+}
+
+
+
+
+export default Home;
